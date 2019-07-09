5c9b7483ff0bbc96350a2e341975db2ff98b1746
MKhursheed <mohsinkd786@gmail.com>
2019-07-09 12:16:10 +0530

    Web Stores

--- arrays.js
+++ arrays.js
@@ -1,3 +1,18 @@
+const sortSample = ()=>{
+    let nums = [47,106,5,2,1];
+    nums = nums.sort((num1,num2)=>{
+        if(num1 > num2){
+            return -1;
+        }else if (num1 < num2){
+            return 1;
+        }else{
+            return 0;
+        }
+    });
+    console.log('Sort Sample');
+    console.log(nums);
+}
+sortSample();
 const emps = [
     {
         id : 1,
@@ -16,10 +31,23 @@ const emps = [
         name : 'Gabbar',
         email: 'gabbar@gg.com',
         organization: 'CTS'
+    },
+    {
+        id : 4,
+        name : 'Adams',
+        email: 'adam@gg.com',
+        organization: 'LnT'
+    },
+    {
+        id : 5,
+        name : 'John',
+        email: 'john@gg.com',
+        organization: 'Target'
     }
 ];
 const loadEmployees= ()=>{
     let rows = '';
+    console.log(emps);
     // traverse the array
     emps.forEach((employee) =>{
         rows += empRow(employee);
@@ -29,7 +57,6 @@ const loadEmployees= ()=>{
 const filterEmps = () =>{
     let rows = '';
     searchByOrg = document.getElementById('search').value;
-
     emps.filter((emp)=>{
         return emp.organization == searchByOrg;
     }).forEach((employee)=>{
@@ -48,10 +75,10 @@ const empRow = (employee)=>{
 const empTable = (rows)=>{
     let records = `<table>
                     <tr>
-                        <th>Id</th>
-                        <th>Name</th>
+                        <th><a href="#" onclick="sortEmployee('ID')">Id</a></th>
+                        <th><a href="#" onclick="sortEmployee('NAME')">Name</a></th>
                         <th>Email</th>
-                        <th>Organization</th>
+                        <th><a href="#" onclick="sortEmployee('ORG')">Organization</a></th>
                     </tr>
                     ${rows}    
                 </table>`;
@@ -68,4 +95,72 @@ const findById = () =>{
     }else{
         alert('No Employees Found with the given Criteria');
     }
+}
+const sortEmployee = (sortBy)=>{
+    let rows = '';
+    let newEmps = emps;
+    if(sortBy == 'ID'){
+        newEmps.sort((emp1,emp2)=>{
+            if(emp1.id > emp2.id){
+                return -1;
+            }else if(emp1.id < emp2.id){
+                return 1;
+            }else{
+                return 0;
+            }         
+        }).forEach((e)=>{
+            rows += empRow(e);
+        });
+        empTable(rows);
+    }else if(sortBy == 'NAME'){
+        newEmps.sort((emp1,emp2)=>{
+             if(emp1.name > emp2.name){
+                 return 1;
+             }else if(emp1.name < emp2.name){
+                return -1;
+            }else{
+                return 0;
+            }        
+        }).forEach((e)=>{
+            rows += empRow(e);
+        });
+        empTable(rows);
+    }else if(sortBy == 'ORG'){
+        newEmps.sort((emp1,emp2)=>{
+        if(emp1.organization > emp2.organization){
+            return 1;
+        }else if(emp1.organization < emp2.organization){
+           return -1;
+       }else{
+           return 0;
+       }       
+    }).forEach((e)=>{
+        rows += empRow(e);
+    });
+    empTable(rows);
+    }
+}
+const localStore=()=>{
+    localStorage.setItem("message","Hello how r u");
+}
+const sessionStore=()=>{
+    sessionStorage.setItem("sessionMessage","Welcome to Session Storage");
+}
+
+const viewWebStore = () =>{
+    if(localStorage.length > 0 ){
+        Object.keys(localStorage)
+        .forEach((key)=>{
+            console.log(localStorage.getItem(key));
+        })
+    }
+    if(sessionStorage.length > 0 ){
+        Object.keys(sessionStorage)
+        .forEach((key)=>{
+            console.log(sessionStorage.getItem(key));
+        })
+    }
+    // clear web stores
+    localStorage.removeItem('message');
+    sessionStorage.removeItem('sessionMessage');
 }
