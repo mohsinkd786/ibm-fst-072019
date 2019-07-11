69ed46b34911a32ba6d2dbf7cdb4a6df0ec2cf2b
MKhursheed <mohsinkd786@gmail.com>
2019-07-11 16:25:33 +0530

    Fetch APi

--- async.js
+++ async.js
@@ -1,23 +1,18 @@
+// list the users
 const getUsers = () =>{
-    const _url = 'https://reqres.in/api/users?page=3';
-    // get data from server
-    /*
-    const res = await fetch(_url);
-    const result = await res.json();
-    console.log(result.data);  */  
-    console.log('Hello');
-
+    const _url = 'http://dummy.restapiexample.com/api/v1/employees';
     fetch(_url,{
         method: 'GET'
     })
     .then(res=>res.json())
     .then((result)=>{
         let rows = '';
-        result.data.forEach((u)=>{
+        console.log(result);
+        result.forEach((u)=>{
             rows += `<tr>
-                        <th>${u.first_name}</th>
-                        <th>${u.email}</th>
-                        <th><img src="${u.avatar}" /></th>
+                        <th>${u.employee_name}</th>
+                        <th>${u.employee_salary}</th>
+                        <th><img src="${u.profile_image}" /></th>
                     </tr>`
         });
         document.getElementById('users').innerHTML = `<table>
@@ -29,4 +24,32 @@ const getUsers = () =>{
                                                             ${rows}
                                                         </table>`;         
     })
+}
+//create a new user
+const createUser = ()=>{
+    const _url = 'http://dummy.restapiexample.com/api/v1/create'
+    fetch(_url,{
+        method: 'POST',
+        body: JSON.stringify( {
+            name: document.getElementById('name').value,
+            salary: document.getElementById('salary').value,
+            age: document.getElementById('age').value
+        })
+    }).catch(e=>{
+        console.log(e);
+    });
+}
+// modify an existing user
+const updateUser = ()=>{
+    const _url = 'http://dummy.restapiexample.com/api/v1/update/6661'
+    fetch(_url,{
+        method: 'PUT',
+        body: JSON.stringify( {
+            name: document.getElementById('name').value,
+            salary: document.getElementById('salary').value,
+            age: document.getElementById('age').value
+        })
+    }).catch(e=>{
+        console.log(e);
+    });
 }
