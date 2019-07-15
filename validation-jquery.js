d50ffb6f0d86879fd1ec46f5dae7198df6bf7d70
MKhursheed <mohsinkd786@gmail.com>
2019-07-15 12:14:01 +0530

    jquery validation

---
+++ validation-jquery.js
@@ -0,0 +1,34 @@
+$(document).ready(()=>{
+    console.log('Validation vai Jquery');
+    $('#register').click(()=>{
+        $.validator.addMethod('emailValidator',(value,elem)=>{
+                let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                return regex.test(value);
+        });
+
+        $('#frm-register').validate({
+            rules:{
+                name: 'required',
+                phone: {
+                    required : true,
+                    min : 100,
+                    max: 1000
+                },
+                email:{
+                    emailValidator: true
+                }
+            },
+            messages:{
+                name : 'Name cannot be left blank',
+                phone: {
+                    required : 'Phone cannot be left blank',
+                    min : 'Phone cannot be less than 100',
+                    max: 'Phone cannot be greater than 1000'
+                },
+                email: {
+                    emailValidator: 'Invalid Email your@email.com'
+                }
+            }
+        });
+    });
+});
