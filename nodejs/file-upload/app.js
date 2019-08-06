07e13124bb0733463f39f55d68841a496360dce4
MKhursheed <mohsinkd786@gmail.com>
2019-08-06 13:52:12 +0530

    File upload via Multer

---
+++ nodejs/file-upload/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const server = express();
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: 'uploads' ,
+    filename: (rq, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+const upload = multer({storage: storage});
+server.get('/app',(rq,rs)=>{
+    rs.sendFile(__dirname +'/app.html');
+});
+server.post('/upload',upload.single('profile'),(rq,rs,next)=>{
+    console.log(rq.file);
+    rs.json({
+        message : "Image Uploaded Successfully"
+    })
+});
+
+server.listen(2011);
