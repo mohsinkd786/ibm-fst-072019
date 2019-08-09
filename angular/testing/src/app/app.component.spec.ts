1cb98f41c62bcf001b48263b519c52a6e741a482
MKhursheed <mohsinkd786@gmail.com>
2019-08-09 11:41:19 +0530

    Testing

--- angular/testing/src/app/app.component.spec.ts
+++ angular/testing/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        UserComponent
       ],
     }).compileComponents();
   }));
